7bd2e3144642455cc3bc46002537e5ee60a91b06
Ian <iewing085@gmail.com>
2023-05-26 10:27:34 -0400

    get starting location

--- typescript/build/index.js
+++ typescript/build/index.js
@@ -1,15 +1,39 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiWlVMVSIsInZlcnNpb24iOiJ2MiIsInJlc2V0X2RhdGUiOiIyMDIzLTA1LTIwIiwiaWF0IjoxNjg1MTA3ODY3LCJzdWIiOiJhZ2VudC10b2tlbiJ9.gLkbHI54zIx2aaOrLvbROS1RN9ndD2wvZfvgIrHYkZAijlMoCchdPUAp64iOVMhUriAfNkR_-RNEnn48oa99d0-4mBlAaAxbKzz9I2fs5lhifa6xLn9-1KUWwLVV0TZcrNCS4TXXLSE8NyLtGwvoww_Tm1oqeHhCfiKtw7H8TSPnhvXKT30gcKV7LZEMFn-P6Fp_QcdnI6KrL29dmquN2hs-RUJcksumk8vSmgnO11DemuNfhmeBN_d5z4FBIGjOWKStCDp2WTdzaRx0KAqKHfR59oyqg3svAISuvck4nkjNeIr5aE53Vz2x0tPklle9vBmv_V4C11cZSj_6Y1SRlQ";
-const options = {
+const requestOptions = {
     headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + token
     },
 };
-agentDetails(options);
-function agentDetails(options) {
-    console.log('hello');
-    fetch('https://api.spacetraders.io/v2/my/agent', options)
+let sectorSymbol = "";
+let systemSymbol = "";
+let waypointSymbol = "";
+getAgentDetails(requestOptions);
+function getAgentDetails(requestOptions) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield fetch('https://api.spacetraders.io/v2/my/agent', requestOptions)
+            .then(response => response.json())
+            .then(data => {
+            sectorSymbol = data.data.headquarters.substring(0, 2);
+            systemSymbol = data.data.headquarters.substring(0, 7);
+            waypointSymbol = data.data.headquarters;
+        })
+            .catch(error => console.error(error));
+        startingLocation(requestOptions, systemSymbol, waypointSymbol);
+    });
+}
+function startingLocation(requestOptions, systemSymbol, waypointSymbol) {
+    fetch(`https://api.spacetraders.io/v2/systems/${systemSymbol}/waypoints/${waypointSymbol}/`, requestOptions)
         .then(response => response.json())
         .then(data => console.log(data))
         .catch(error => console.log(error));
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,16 +1,30 @@
-const token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiWlVMVSIsInZlcnNpb24iOiJ2MiIsInJlc2V0X2RhdGUiOiIyMDIzLTA1LTIwIiwiaWF0IjoxNjg1MTA3ODY3LCJzdWIiOiJhZ2VudC10b2tlbiJ9.gLkbHI54zIx2aaOrLvbROS1RN9ndD2wvZfvgIrHYkZAijlMoCchdPUAp64iOVMhUriAfNkR_-RNEnn48oa99d0-4mBlAaAxbKzz9I2fs5lhifa6xLn9-1KUWwLVV0TZcrNCS4TXXLSE8NyLtGwvoww_Tm1oqeHhCfiKtw7H8TSPnhvXKT30gcKV7LZEMFn-P6Fp_QcdnI6KrL29dmquN2hs-RUJcksumk8vSmgnO11DemuNfhmeBN_d5z4FBIGjOWKStCDp2WTdzaRx0KAqKHfR59oyqg3svAISuvck4nkjNeIr5aE53Vz2x0tPklle9vBmv_V4C11cZSj_6Y1SRlQ";
-const options = {
+const token: string = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiWlVMVSIsInZlcnNpb24iOiJ2MiIsInJlc2V0X2RhdGUiOiIyMDIzLTA1LTIwIiwiaWF0IjoxNjg1MTA3ODY3LCJzdWIiOiJhZ2VudC10b2tlbiJ9.gLkbHI54zIx2aaOrLvbROS1RN9ndD2wvZfvgIrHYkZAijlMoCchdPUAp64iOVMhUriAfNkR_-RNEnn48oa99d0-4mBlAaAxbKzz9I2fs5lhifa6xLn9-1KUWwLVV0TZcrNCS4TXXLSE8NyLtGwvoww_Tm1oqeHhCfiKtw7H8TSPnhvXKT30gcKV7LZEMFn-P6Fp_QcdnI6KrL29dmquN2hs-RUJcksumk8vSmgnO11DemuNfhmeBN_d5z4FBIGjOWKStCDp2WTdzaRx0KAqKHfR59oyqg3svAISuvck4nkjNeIr5aE53Vz2x0tPklle9vBmv_V4C11cZSj_6Y1SRlQ";
+const requestOptions: RequestInit = {
     headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + token
       },
 }
+let sectorSymbol: string = "";
+let systemSymbol: string = "";
+let waypointSymbol: string = "";
 
-agentDetails(options);
+getAgentDetails (requestOptions);
 
-function agentDetails(options: RequestInit): void {    
-console.log('hello');
-    fetch('https://api.spacetraders.io/v2/my/agent', options)
+async function getAgentDetails (requestOptions: RequestInit): Promise<void> {    
+    await fetch('https://api.spacetraders.io/v2/my/agent', requestOptions)
+    .then(response => response.json())
+    .then(data => {
+        sectorSymbol = data.data.headquarters.substring(0, 2);
+        systemSymbol = data.data.headquarters.substring(0, 7);
+        waypointSymbol = data.data.headquarters;               
+    })
+    .catch(error => console.error(error));
+    startingLocation(requestOptions, systemSymbol, waypointSymbol);
+}
+
+function startingLocation(requestOptions: RequestInit, systemSymbol: string, waypointSymbol: string): void {    
+    fetch(`https://api.spacetraders.io/v2/systems/${systemSymbol}/waypoints/${waypointSymbol}/`, requestOptions)
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.log(error));
